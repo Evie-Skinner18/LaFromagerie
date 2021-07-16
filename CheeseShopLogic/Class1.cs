17b0ad8c15e56dc182b0398fb6c458fb58fa7833
Evie <evie.skinner@e-days.com>
2021-07-16 09:04:58 +0100

    Initial commit

---
+++ CheeseShopLogic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CheeseShopLogic
+{
+    public class Class1
+    {
+    }
+}
---
+++ CheeseShopUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CheeseShopUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue a la fromagerie!");
+        }
+    }
+}
