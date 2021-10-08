484442e8b7998e55fe905bf896ae0b5626990a1f
Taha Ghadirian <taha.ghadirian@outlook.com>
2021-10-08 13:18:30 +0330

    Fix XML Docs.

--- CheeseShopLogic/Subscriptions/PremiumCheeseSubscription.cs
+++ CheeseShopLogic/Subscriptions/PremiumCheeseSubscription.cs
@@ -13,43 +13,42 @@ public class PremiumCheeseSubscription : ISubscription
     private string _name { get; set; }
     private decimal _monthlyPrice { get; set; }
 
-    /// <exception cref="System.NotImplementedException"></exception>
     public void Attach(IObserver observer)
     {
         throw new System.NotImplementedException();
     }
 
-    /// <exception cref="System.NotImplementedException"></exception>
+    /// <inheritdoc/>
     public void ChangeName(string newName)
     {
         throw new System.NotImplementedException();
     }
 
-    /// <exception cref="System.NotImplementedException"></exception>
+    /// <inheritdoc/>
     public void ChangePrice(decimal newPrice)
     {
         throw new System.NotImplementedException();
     }
 
-    /// <exception cref="System.NotImplementedException"></exception>
+    /// <inheritdoc/>
     public void Detach(IObserver observer)
     {
         throw new System.NotImplementedException();
     }
 
-    /// <exception cref="System.NotImplementedException"></exception>
+    /// <inheritdoc/>
     public string GetName()
     {
         throw new System.NotImplementedException();
     }
 
-    /// <exception cref="System.NotImplementedException"></exception>
+    /// <inheritdoc/>
     public decimal GetPrice()
     {
         throw new System.NotImplementedException();
     }
 
-    /// <exception cref="System.NotImplementedException"></exception>
+    /// <inheritdoc/>
     public void NotifyObservers()
     {
         throw new System.NotImplementedException();
